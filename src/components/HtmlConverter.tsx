394219b621ec40d688dde8fb1394235e407462ad
Maliheh Norouzi <malihe.noroozi64@yahoo.com>
2025-04-23 09:02:21 +0200

    Used react-markdown for converting markdown to html

--- src/components/HtmlConverter.tsx
+++ src/components/HtmlConverter.tsx
@@ -2,8 +2,10 @@ import ReactMarkdown from "react-markdown";
 
 export function HtmlConverter({ markdown }: { markdown: string }) {
   return (
-    <ReactMarkdown className="flex-grow overflow-auto p-4 bg-gray-100 rounded-md">
-      {markdown}
-    </ReactMarkdown>
+    <div className="flex-grow overflow-auto p-4 bg-gray-100 rounded-md">
+      <ReactMarkdown className="prose prose-headings:mt-8 prose-headings:font-semibold prose-headings:text-black prose-h1:text-5xl prose-h2:text-4xl prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-xl prose-h6:text-lg dark:prose-headings:text-white">
+        {markdown}
+      </ReactMarkdown>
+    </div>
   );
 }
